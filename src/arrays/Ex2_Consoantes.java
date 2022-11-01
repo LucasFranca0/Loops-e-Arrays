cf44071789b034babbdb18a49c05c99293cfa574
Lucas C. França <111810585+LucasFranca0@users.noreply.github.com>
2022-11-01 17:03:03 -0300

    Update Ex2_Consoantes.java

--- src/arrays/Ex2_Consoantes.java
+++ src/arrays/Ex2_Consoantes.java
@@ -3,7 +3,7 @@ package arrays;
 import java.util.Scanner;
 
 /*
-Façça um programa que leia um vetor de 6 caracteres,
+Faça um programa que leia um vetor de 6 caracteres,
 e diga quantas consoantes foram lidas.
 Imprima as consoantes.
  */
